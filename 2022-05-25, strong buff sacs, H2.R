239f4ee4a17c5ca098b5cf211f5d544557c28a75
Evan McKenzie <evan.mcken@gmail.com>
2022-05-26 19:19:44 -0700

    updates and strong buff sac control for H2 production

--- 2022-05-25, strong buff sacs, H2.R
+++ 2022-05-25, strong buff sacs, H2.R
@@ -25,23 +25,23 @@ head(nox.times, n = 24)
 
 # get all the OCP .csv data files together into a df 
 list_of_OCP.CSVs <- 
-  list.files(path = "~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-18/OCPs",
+  list.files(path = "~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-25, strong buff H2 control/OCPs",
              recursive = TRUE,
              pattern = "\\.csv$",
              full.names = TRUE)
 
 OCPs.df <- readr::read_csv(list_of_OCP.CSVs, id = "OCP.files") %>%
   mutate(...3 = NULL)
-head(OCPs.df, n = 10)
+view(OCPs.df, n = 10)
 
 ######################     set working directory to folder with image files     ######################
 # sac size data image file times
-setwd("~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-18/2022_05_18")
+setwd("~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-25, strong buff H2 control/2022_05_25")
 
 
 ######################     import csv w/ measurements (airsac sizes)    ######################
 data <- 
-  read_csv("~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-18/2022-05-18, Areas.csv") %>%
+  read_csv("~/student_documents/UBC/Research/pH_stat/with_a_sac_(prelim)/2022-05-25, strong buff H2 control/2022-05-25, Areas-trimmed.csv") %>%
   mutate(...1 = NULL) %>%
   mutate(Mean = NULL)
 print(data)
@@ -68,11 +68,11 @@ OCPs_with_calcs <- OCPs.df %>%
   group_by(OCP.files) %>% # can be off by the fraction of a second diff between OCP seconds start time
   mutate(median_V = median(V)) %>%
   mutate(exp_start = (exp.times$seconds[1])) %>%
-  mutate(substring(OCP.files, 101)) %>% #new column with ocp.files dropping the path before the file name (here position 101)
-  mutate(substring(substring(OCP.files, 101), 1, 18)) %>% ## drop file extension (works as long as the OCP # has three digits)
-  rename(OCP = 'substring(substring(OCP.files, 101), 1, 18)') %>% # fix stupid column name
+  mutate(substring(OCP.files, 125)) %>% #new column with ocp.files dropping the path before the file name (here position 101)
+  mutate(substring(substring(OCP.files, 125), 1, 18)) %>% ## drop file extension (works as long as the OCP # has three digits)
+  rename(OCP = 'substring(substring(OCP.files, 125), 1, 18)') %>% # fix stupid column name
   mutate(OCP = paste(OCP, "nox", sep = ".")) %>% ## now OCP should = the nox.times$nox_files so they can be linked later
-  mutate('substring(OCP.files, 101)' = NULL) %>%
+  mutate('substring(OCP.files, 125)' = NULL) %>%
   ungroup() %>%
   group_by(OCP) %>%
   mutate(OCP.files = NULL) %>%
@@ -88,7 +88,7 @@ head(OCPs_with_calcs)
 #make a data frame with just median_V and exp.time.point from OCPs_with_calcs
 OCP_V_time <- OCPs_with_calcs %>%
   ungroup() %>%
-  mutate(pH = (median_V + 0.332)/0.054) %>% #old calibration
+  mutate(pH = (median_V + 0.3542)/0.054) %>% #old calibration
   mutate(seconds = NULL) %>%
   mutate(V = NULL) %>%
   mutate(minutes_from_OCP_start = NULL) %>%
@@ -101,10 +101,10 @@ OCP_V_time <- OCPs_with_calcs %>%
 head(OCP_V_time, n = 12)
 
 # repeat each of the file info values for how many sacs are in each image (i.e. in each file)
-rep.file.time <- rep(exp.times$file.time, each = 4)
-rep.jpg_files <- rep(exp.times$jpg_files, each = 4)
-rep.seconds <- rep(exp.times$seconds, each = 4)
-rep.minutes_from_start <- rep(exp.times$minutes_from_start, each = 4)
+rep.file.time <- rep(exp.times$file.time, each = 3)
+rep.jpg_files <- rep(exp.times$jpg_files, each = 3)
+rep.seconds <- rep(exp.times$seconds, each = 3)
+rep.minutes_from_start <- rep(exp.times$minutes_from_start, each = 3)
 
 # make a data frame of the above repeated info and add the area data
 file.reps <- data.frame(rep.file.time) %>%
@@ -113,17 +113,18 @@ file.reps <- data.frame(rep.file.time) %>%
   mutate(rep.minutes_from_start) %>%
   mutate(data, Area) %>% #get areas from imported .csv
   mutate(sac = 
-           rep(c("1","2","3","4"), times = nrow(exp.times))) %>% #sac ID column
+           rep(c("1","2","3"), times = nrow(exp.times))) %>% #sac ID column
   group_by(sac) %>%
   mutate(
     area.pct.change = ((Area - Area[1]) / Area[1])
     *100) %>%
   ungroup()
 
-file.reps <- merge(file.reps, OCP_V_time, all = TRUE) #%>% # insert rows for the pH data points
-  #filter(rep.minutes_from_start >= 0)
+# insert rows for the pH data points
+file.reps <- merge(file.reps, OCP_V_time, all = TRUE) %>% 
+  filter(rep.minutes_from_start < 1222)
 
-head(file.reps, n = 50)
+tail(file.reps, n = 20)
 
 
 ########################     plotting and stats    ########################
@@ -135,38 +136,41 @@ ggplot(data = file.reps,
   geom_point(aes(y= area.pct.change)) +
   geom_line(aes(y= area.pct.change)) +
   scale_colour_discrete(na.translate = F) +
-  geom_point(aes(y= (pH *30) - 184.2054 - 14), # magnify relative to main axis, translate points so first point = zero, translate down for fit -> see scale_y_continuous
+  geom_point(aes(y= (pH *10) - 59.95673 + 2.5), # magnify relative to main axis, translate points so first point = zero, translate for fit -> see scale_y_continuous
              size = 4.4,
              colour = "orangered3") +
   scale_y_continuous(sec.axis = 
-                       sec_axis(~./30 + 6.14018 + (14/30), # / by magnify factor, + start pH, + (vert translation/ mag factor)
-                                name = "pH", 
-                                breaks = c(6.25, 6.5, 6.75, 7, 7.25))) +  
-  geom_vline(xintercept = 49.9, colour = "black") + 
-  geom_vline(xintercept = 57.7, colour = "grey") +
-  geom_vline(xintercept = 64.9, colour = "grey") +
-  geom_vline(xintercept = 364.9, colour = "black") + 
-  annotate("rect", xmin=126.45, xmax=126.45 + 13.578, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=145.6333, xmax=145.6333 + 13.626, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=165.55, xmax=165.55  + 9.79125, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=179.55, xmax=179.55  + 1.03657, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=186.783, xmax=186.783  + 1.2828, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=250.65, xmax=250.65  + 13.63425, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=272.8, xmax=272.8  + 13.6398, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=291.16, xmax=291.16  + 13.661, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
-  annotate("rect", xmin=305.08, xmax=305.08  + 9.6026, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
+                       sec_axis(~./10 + 5.995673 - (2.5/10), # / by magnify factor, + start pH, + (vert translation/ mag factor)
+                                name = "pH")) +
+  geom_hline(yintercept = 0.23, colour = "blue") +
+  geom_hline(yintercept = 0.73, colour = "green") +
+  geom_hline(yintercept = 0.26, colour = "lightcoral") +
+  #annotate("rect", xmin=126.45, xmax=126.45 + 13.578, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
+  #annotate("rect", xmin=145.6333, xmax=145.6333 + 13.626, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
+  #annotate("rect", xmin=165.55, xmax=165.55  + 9.79125, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
+  #annotate("rect", xmin=179.55, xmax=179.55  + 1.03657, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
+ # annotate("rect", xmin=186.783, xmax=186.783  + 1.2828, ymin=-16, ymax=22, fill = "green3", alpha=0.2) +
   labs(x = "Minutes", 
        y = "Sac area % change") +
   theme_classic() +
   theme(axis.ticks.length = unit(-1, "mm")) +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 24)) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 15)) +
   theme(legend.position = c(0.07, 0.8))
+
+
+
+
+
+####################   still copy of old   ###################
+
+
+
 
 
 #subset of experiment times (could be plateau or linear increase region)
 time.subset <- file.reps %>%
   filter(rep.minutes_from_start > 255, rep.minutes_from_start < 320) #%>% 
-  #mutate(area.pct.change = replace(area.pct.change, sac == 4, NA)) # remove damaged/ unresponsive sac from analysis
+#mutate(area.pct.change = replace(area.pct.change, sac == 4, NA)) # remove damaged/ unresponsive sac from analysis
 
 print(time.subset)
 
